a7c5303331d68b31733fc9e3ee1702f98fe82f44
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-05-12 10:46:38 +0200

    sum of multiples

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,9 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!("Sum the multiples of all of {factors:?} which are less than {limit}")
+    let mut sum = 0;
+    for i in 1..limit {
+        if factors.iter().any(|&x| x != 0 && i % x == 0) {
+            sum += i;
+        }
+    }
+    sum
 }
